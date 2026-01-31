230f3680a4d19b10d200a4d684976f47572b6d74
Umaji Bagal <bagalumaji@gmail.com>
2026-01-31 11:11:46 +0530

    updated code

--- src/main/java/com/bagal/LC26RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/bagal/LC26RemoveDuplicatesFromSortedArray.java
@@ -2,6 +2,13 @@ package com.bagal;
 
 public class LC26RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-        return 0;
+        int cnt=1;
+        for (int i = 1; i <nums.length ; i++) {
+            if(nums[i]!=nums[i-1]){
+                nums[cnt] = nums[i];
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -1,7 +1,15 @@
 package tests;
 
+import com.bagal.LC26RemoveDuplicatesFromSortedArray;
+
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+//        int [] arr = {0,0,1,1,1,2,2,3,3,4};
+        int [] arr = {1,1,2};
+        LC26RemoveDuplicatesFromSortedArray d = new LC26RemoveDuplicatesFromSortedArray();
+        int cnt = d.removeDuplicates(arr);
+        for (int i = 0; i < cnt; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
