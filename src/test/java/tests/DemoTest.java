6b3d42a88cd1d4a99b02543508fe3bca90694fd2
Umaji Bagal <bagalumaji@gmail.com>
2026-02-03 22:16:41 +0530

    updated

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -1,15 +1,12 @@
 package tests;
 
+import com.bagal.LC217ContainsDuplicateInArray;
 import com.bagal.LC26RemoveDuplicatesFromSortedArray;
 
 public class DemoTest {
     public static void main(String[] args) {
 //        int [] arr = {0,0,1,1,1,2,2,3,3,4};
         int [] arr = {1,1,2};
-        LC26RemoveDuplicatesFromSortedArray d = new LC26RemoveDuplicatesFromSortedArray();
-        int cnt = d.removeDuplicates(arr);
-        for (int i = 0; i < cnt; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.println(LC217ContainsDuplicateInArray.containsDuplicate(arr));
     }
 }
