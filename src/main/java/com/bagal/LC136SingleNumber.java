7e33fa67d5820edb27fd2a9d5dba37dc02944436
Umaji Bagal <bagalumaji@gmail.com>
2026-02-05 22:26:35 +0530

    leet code 136. Single Number

---
+++ src/main/java/com/bagal/LC136SingleNumber.java
@@ -0,0 +1,21 @@
+package com.bagal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC136SingleNumber {
+    /// 1st approach
+    public static int singleNumber1(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int result = nums[0];
+        for (int num:nums){
+            if(map.get(num)==1){
+                result = num;
+            }
+        }
+        return result;
+    }
+}
