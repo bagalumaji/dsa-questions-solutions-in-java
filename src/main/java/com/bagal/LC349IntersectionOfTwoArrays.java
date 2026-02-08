47a42acccd4f38257818a96f1839b0037b365f74
Umaji Bagal <bagalumaji@gmail.com>
2026-02-08 12:01:24 +0530

    leet code 349. Intersection of Two Arrays

--- src/main/java/com/bagal/LC349IntersectionOfTwoArrays.java
+++ src/main/java/com/bagal/LC349IntersectionOfTwoArrays.java
@@ -26,4 +26,25 @@ public class LC349IntersectionOfTwoArrays {
         }
         return Arrays.copyOfRange(result,0,k);
     }
+    // way 2
+    public static int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num:nums1){
+            set1.add(num);
+        }
+        for (int num:nums2){
+            if(set1.contains(num)){
+                set2.add(num);
+            }
+        }
+
+        int []res = new int[set2.size()];
+        int i=0;
+        for (int num:set2){
+            res[i++] = num;
+        }
+        return res;
+    }
 }
