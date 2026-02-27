378031d3543a0c4fad0ce292d5fdd44626eed899
Umaji Bagal <bagalumaji@gmail.com>
2026-02-27 07:29:47 +0530

    leet code 387. First Unique Character in a String

--- src/main/java/com/bagal/LC387FirstUniqueCharacterInAString.java
+++ src/main/java/com/bagal/LC387FirstUniqueCharacterInAString.java
@@ -1,7 +1,16 @@
 package com.bagal;
 
 public class LC387FirstUniqueCharacterInAString {
-    public int firstUniqChar(String s) {
+    //first approach
+    public static int firstUniqChar(String s) {
+        int i=0;
+        while(i < s.length()){
+            char ch = s.charAt(i);
+            if(s.indexOf(ch) == s.lastIndexOf(ch)){
+                return i;
+            }
+            i++;
+        }
         return -1;
     }
 }
