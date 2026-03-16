092b3e40b3ab3f912bdb4c7c67d15283798ffdd6
umaji bagal <umaji.bagal@testingmavens.com>
2026-03-16 22:02:22 +0530

    Sort Positive, Negative, And Zero //input - 0, 2, 1, 0, -9, 0, 5, 0, 6, -2, 7, -5, 0, 3 //output- 0, 0, 0, 0, 0, -2, -5, -9, 1, 2, 3, 5, 6, 7

---
+++ src/main/java/com/bagal/interview/SortPositiveNegativeAndZero.java
@@ -0,0 +1,30 @@
+package com.bagal.interview;
+
+import java.util.Arrays;
+
+//input - 0, 2, 1, 0, -9, 0, 5, 0, 6, -2, 7, -5, 0, 3
+//output- 0, 0, 0, 0, 0, -2, -5, -9, 1, 2, 3, 5, 6, 7
+public class SortPositiveNegativeAndZero {
+    public static int[] sortPositiveNegativeAndZero(int []arr){
+        Arrays.sort(arr);
+        int end = arr.length-1;
+        int start = 0;
+        while (arr[end]!=0){
+            end--;
+        }
+        while (start<end){
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int []arr = sortPositiveNegativeAndZero(new int[]{0,2,1,0,-9,0,5,0,6,-2,7,-5,0,3});
+        System.out.println(Arrays.toString(arr));
+        //System.out.println(arr);
+    }
+}
